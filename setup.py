cb5e8a1a36af33e8a7b072bce7dec88f311a5d57
Jinpyo Hong <hojp7874@gmail.com>
2021-08-09 22:41:50 +0900

    release 1

--- setup.py
+++ setup.py
@@ -8,15 +8,9 @@ setup(
     author='hongjinpyo',
     author_email='hojp7874@gmail.com',
     license='hongjinpyo',
-    packages=find_packages(exclude=[]),
+    packages=find_packages(exclude=['tests']),
     install_requires=[
-        'beautifulsoup4==4.9.3',
         'bs4==0.0.1',
-        'certifi==2021.5.30',
-        'chardet==4.0.0',
-        'idna==2.10',
-        'requests==2.25.1',
-        'soupsieve==2.2.1',
-        'urllib3==1.26.5',
+        'requests==2.26.0',
     ]
 )
